87a46d2a295e8dd0279b6443de58db75a57c22ec
Ikrum Hossain <ikrum.bd@gmail.com>
2016-04-26 21:40:59 +0600

    loop

--- js/loop-es6.js
+++ js/loop-es6.js
@@ -1,4 +1,61 @@
-let arr = [1,2,3];
 
-for (let n of arr) {
-  console.log(n);
+//Iterating over an Array
+(function(){
+	let myArray = [1, 2, 3];
+	for (let value of myArray) {
+	  console.log(value);
+	  // 1
+		// 2
+		// 3
+	}
+})();
+
+
+// Iterating over a String
+(function(){
+	let myString = "abc";
+	for (let value of myString) {
+	  console.log(value);
+	}
+	// "a"
+	// "b"
+	// "c
+})();
+
+
+/*
+ * Difference between for...of and for...in
+ *
+ * for..of takes the value from the iteretor and for..in takes the index
+ * When you need only the item for the iterator you can use for..of lopp
+ * If index or key is required then you could use regular for loop or for..in loop
+ */
+
+// when index is required, or you want to use object like associative array
+(function(){
+	var opts = {
+		size: 20,
+		flag: "OK"
+	};
+	for (let key in opts){
+		// index is required
+		if(key == "size"){
+			// do something with key
+		}
+
+		// use like associative array
+		console.log( opts[key] );
+	}
+})();
+
+// when index is not required, we just need the value
+(function(){
+	var foo = ["a","b", "c"];
+	for(let item of foo){
+		console.log(item);
+		//a
+		//b
+		//c
+	}
+})();
+  
